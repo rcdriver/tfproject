818391b92f2c9a87ed5cd3de98803e71bcff4faf
rohitchauhan <rohitchauhanit@gmail.com>
2022-11-24 13:42:45 +0530

    commit-3

--- project/1-terraform-block.tf
+++ project/1-terraform-block.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.3.0"   // Terraform AWS Plugins version
+#      version = "4.3.0"   // Terraform AWS Plugins version
     }
   }
 }
--- project/outputs.tf
+++
@@ -1,8 +0,0 @@
-output vpc1_op {
-  value = aws_vpc.vpc1 // this will list all the attributes
-}
-
-output subnet1_op {
-  value = aws_vpc.subnet1 // this will list all the attributes
-}
-
